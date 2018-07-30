93d0a4dc6762014e478a695fb3922e473fa50f19
dbhrockzz <dbhrockzz1@gmail.com>
2018-07-30 14:14:16 +0530

    Base Templates dded for Candidate App

--- CVPrioritize/urls.py
+++ CVPrioritize/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^posts/',include('resume_ranker.urls')),
+    url(r'^candidate/',include('candidate.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- candidate/views.py
+++ candidate/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'candidate/index.html', {})
+

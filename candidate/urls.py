2e5e0a5f47749cec9fe2c1be348548db9f619bf4
dbhrockzz <dbhrockzz1@gmail.com>
2018-08-01 12:58:07 +0530

    Candidate URLs Updated

--- candidate/urls.py
+++ candidate/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.index, name = "home"),
+    url(r'^', views.index, name = "home"),
+    url(r'^editdetails/', views.editdetails, name='editdetails'),
 ]
-

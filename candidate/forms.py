7047a40fccb7bec79cf92fead0490b88f53a6d29
dbhrockzz <dbhrockzz1@gmail.com>
2018-08-02 09:11:23 +0530

    Base and Forms of Candidate Module Modified

--- candidate/forms.py
+++ candidate/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from django.conf import settings
 
 class EditDetails(forms.Form):
+    EXPERIENCE_CHOICES = (
+    ('1-3','1-3'),
+    ('3-6','3-6'),
+    ('6-9','6-9'),
+    ('9-12','9-12'),
+    ('12-15','12-15')
+    )
 
     use_required_attribute=False
     name=forms.CharField(min_length=4,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Name'}))
@@ -9,7 +16,7 @@ class EditDetails(forms.Form):
     phone=forms.CharField(min_length=10,max_length=10,widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Phone Number'}))
     address=forms.CharField(min_length=20,max_length=2000,widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Address'}))
     pincode=forms.CharField(min_length=6,max_length=6,widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'PIN'}))
-    experience=forms.ChoiceField(choices=[(x, x) for x in range(1, 32)])
+    experience=forms.ChoiceField(choices=EXPERIENCE_CHOICES,widget=forms.Select(attrs={'class':'form-control'}))
     Qualifiction=forms.CharField(min_length=20,max_length=2000,widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Qualification'}))
     Skills=forms.MultiValueField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Skills'}))
 

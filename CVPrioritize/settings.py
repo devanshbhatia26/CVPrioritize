fb90c72a9960e9f246fca09c66502616e3c62202
dbhrockzz <dbhrockzz1@gmail.com>
2018-08-08 11:21:40 +0530

    Database conf

--- CVPrioritize/settings.py
+++ CVPrioritize/settings.py
@@ -79,12 +79,25 @@ WSGI_APPLICATION = 'CVPrioritize.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'cvprioritize_database',
+            'USER': 'python_user',
+            'PASSWORD': '2plus1=3',
+            'HOST': 'filairdbinstance.cfqfv3zk86qg.ap-south-1.rds.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
+    
 
 
 # Password validation
